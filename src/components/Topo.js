a3f5adb1c592177f4c4618605cdf42f5bd44b98b
FernandoM52 <fernandomartins.contato@outlook.com>
2023-03-01 23:27:10 -0300

    Adicinado Logo com styled components

---
+++ src/components/Topo.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import logo from "../assets/imgs/logo.png";
+export default function Topo() {
+    return (
+        <H1>
+            <img src={logo} alt="Zap Recall"></img>
+            <>ZapRecall</>
+        </H1>
+    );
+}
+
+const H1 = styled.h1`
+color: #FFFFFF;
+width: 255px;
+font-family: 'Righteous', cursive;
+font-weight: 400;
+font-size: 36px;
+line-height: 44px;
+padding-top: 42px;
+display: flex;
+img{
+    width: 52px;
+    height: 60px;
+    margin-right: 20px;
+}
+`
